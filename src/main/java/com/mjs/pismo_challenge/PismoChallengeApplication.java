3bfb38db9ba2c3544ecb5fa24758469097520883
michel-jader <michel.jader.de.souza@gmail.com>
2026-01-28 18:12:22 -0300

    init

---
+++ src/main/java/com/mjs/pismo_challenge/PismoChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.mjs.pismo_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PismoChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PismoChallengeApplication.class, args);
+	}
+
+}
